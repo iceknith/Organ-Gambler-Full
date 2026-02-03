87d915efb673cbc8953e003374f5652616413d70
Iceknith <dimi.ma@laposte.net>
2026-02-03 15:39:37 +0100

    spelling mistake fix

--- src/objects/coins/coinLoader.gd
+++ src/objects/coins/coinLoader.gd
@@ -3,9 +3,9 @@ extends Node
 var coins:Dictionary
 
 func _ready() -> void:
-	load_organs()
+	load_coins()
 
-func load_organs() -> void:
+func load_coins() -> void:
 	var dir := DirAccess.open("res://src/objects/coins/list")
 	if dir == null: printerr("Could not open folder res://src/objects/coins/list"); return
 	dir.list_dir_begin()
@@ -13,7 +13,7 @@ func load_organs() -> void:
 		var coin:Coin = load(dir.get_current_dir() + "/" + file)
 		coins.set(coin.name, dir.get_current_dir() + "/" + file)
 
-func get_organ(name:String) -> Organ:
+func get_coins(name:String) -> Organ:
 	if (coins.has(name)): return load(coins.get(name))
 	printerr("No coins matching the name " + name + " found !")
 	return null
